b4dfe43e020138a58b21bd2ea9f9d32de4a33b56
psychopurp <44772640+psychopurp@users.noreply.github.com>
2023-09-07 10:49:50 +0800

    chore: complete the test for create_bots

--- crates/delta/src/routes/bots/create.rs
+++ crates/delta/src/routes/bots/create.rs
@@ -199,8 +199,11 @@ mod test {
 
         assert_eq!(response.status(), Status::Ok);
 
-        let bot: v0::Bot = response.into_json().await.expect("`Bot`");
-        assert!(harness.db.fetch_bot(&bot.id).await.is_ok());
+        let bot_response: v0::Bot = response.into_json().await.expect("`Bot`");
+        let bot = harness.db.fetch_bot(&bot_response.id).await.unwrap();
+
+        assert!(bot.server_invite.is_some());
+        assert!(bot.default_server.is_some());
     }
 
     #[test]
