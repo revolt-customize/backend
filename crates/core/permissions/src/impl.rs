210649ae45c3e9006ad0b21519aec0db56e94c69
psychopurp <44772640+psychopurp@users.noreply.github.com>
2023-09-08 15:03:11 +0800

    fix: disable permission checks for message

--- crates/core/permissions/src/impl.rs
+++ crates/core/permissions/src/impl.rs
@@ -63,70 +63,72 @@ pub async fn calculate_server_permissions<P: PermissionQuery>(query: &mut P) -> 
 }
 
 /// Calculate permissions against a channel
-pub async fn calculate_channel_permissions<P: PermissionQuery>(query: &mut P) -> PermissionValue {
-    if query.are_we_privileged().await {
-        return ChannelPermission::GrantAllSafe.into();
-    }
+pub async fn calculate_channel_permissions<P: PermissionQuery>(_query: &mut P) -> PermissionValue {
+    ChannelPermission::GrantAllSafe.into()
 
-    match query.get_channel_type().await {
-        ChannelType::SavedMessages => {
-            if query.do_we_own_the_channel().await {
-                DEFAULT_PERMISSION_SAVED_MESSAGES.into()
-            } else {
-                0_u64.into()
-            }
-        }
-        ChannelType::DirectMessage => {
-            if query.are_we_part_of_the_channel().await {
-                query.set_recipient_as_user().await;
+    // if query.are_we_privileged().await {
+    //     return ChannelPermission::GrantAllSafe.into();
+    // }
 
-                let permissions = calculate_user_permissions(query).await;
-                if (permissions & UserPermission::SendMessage as u32)
-                    == UserPermission::SendMessage as u32
-                {
-                    (*DEFAULT_PERMISSION_DIRECT_MESSAGE).into()
-                } else {
-                    (*DEFAULT_PERMISSION_VIEW_ONLY).into()
-                }
-            } else {
-                0_u64.into()
-            }
-        }
-        ChannelType::Group => {
-            if query.do_we_own_the_channel().await {
-                ChannelPermission::GrantAllSafe.into()
-            } else if query.are_we_part_of_the_channel().await {
-                (*DEFAULT_PERMISSION_VIEW_ONLY
-                    | query.get_default_channel_permissions().await.allow)
-                    .into()
-            } else {
-                0_u64.into()
-            }
-        }
-        ChannelType::ServerChannel => {
-            query.set_server_from_channel().await;
+    // match query.get_channel_type().await {
+    //     ChannelType::SavedMessages => {
+    //         if query.do_we_own_the_channel().await {
+    //             DEFAULT_PERMISSION_SAVED_MESSAGES.into()
+    //         } else {
+    //             0_u64.into()
+    //         }
+    //     }
+    //     ChannelType::DirectMessage => {
+    //         if query.are_we_part_of_the_channel().await {
+    //             query.set_recipient_as_user().await;
 
-            if query.are_we_a_member().await {
-                let mut permissions = calculate_server_permissions(query).await;
-                permissions.apply(query.get_default_channel_permissions().await);
+    //             let permissions = calculate_user_permissions(query).await;
+    //             if (permissions & UserPermission::SendMessage as u32)
+    //                 == UserPermission::SendMessage as u32
+    //             {
+    //                 (*DEFAULT_PERMISSION_DIRECT_MESSAGE).into()
+    //             } else {
+    //                 (*DEFAULT_PERMISSION_VIEW_ONLY).into()
+    //             }
+    //         } else {
+    //             0_u64.into()
+    //         }
+    //     }
+    //     ChannelType::Group => {
+    //         if query.do_we_own_the_channel().await {
+    //             ChannelPermission::GrantAllSafe.into()
+    //         } else if query.are_we_part_of_the_channel().await {
+    //             (*DEFAULT_PERMISSION_VIEW_ONLY
+    //                 | query.get_default_channel_permissions().await.allow)
+    //                 .into()
+    //         } else {
+    //             0_u64.into()
+    //         }
+    //     }
+    //     ChannelType::ServerChannel => {
+    //         query.set_server_from_channel().await;
 
-                for role_override in query.get_our_channel_role_overrides().await {
-                    permissions.apply(role_override);
-                }
+    //         if query.are_we_a_member().await {
+    //             let mut permissions = calculate_server_permissions(query).await;
+    //             permissions.apply(query.get_default_channel_permissions().await);
 
-                if query.are_we_timed_out().await {
-                    permissions.restrict(*ALLOW_IN_TIMEOUT);
-                }
+    //             for role_override in query.get_our_channel_role_overrides().await {
+    //                 permissions.apply(role_override);
+    //             }
 
-                if !permissions.has_channel_permission(ChannelPermission::ViewChannel) {
-                    permissions.revoke_all();
-                }
-                
-                permissions
-            } else {
-                0_u64.into()
-            }
-        }
-        ChannelType::Unknown => 0_u64.into(),
-    }
+    //             if query.are_we_timed_out().await {
+    //                 permissions.restrict(*ALLOW_IN_TIMEOUT);
+    //             }
+
+    //             if !permissions.has_channel_permission(ChannelPermission::ViewChannel) {
+    //                 permissions.revoke_all();
+    //             }
+
+    //             permissions
+    //         } else {
+    //             0_u64.into()
+    //         }
+    //     }
+    //     ChannelType::Unknown => 0_u64.into(),
+    // }
 }
